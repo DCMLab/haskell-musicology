c4850ac5bdaf1480d2d4006f32f052663fa2dbcf
Christoph Finkensiep <chfin@chfin.de>
2019-12-06 15:42:15 +0100

    better options for musicxml2json

--- musicology-musicxml/app/tojson/Main.hs
+++ musicology-musicxml/app/tojson/Main.hs
@@ -1,14 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
-import Musicology.MusicXML
-import Data.Aeson
-import qualified Data.ByteString.Lazy as B
+import           Data.Aeson
+import qualified Data.Aeson.Encoding           as E
+import qualified Data.Aeson.Encode.Pretty      as P
+import qualified Data.ByteString.Lazy          as B
+import           Options.Applicative
 
-import Musicology.Types
+import           Musicology.MusicXML
+import           Musicology.Types
 
-notateNoteId :: Notation p => NoteId p t i -> NoteId String t i
-notateNoteId (NoteId (Pitch p) on off id) = NoteId (Pitch (showNotation p)) on off id
+data Opts = Opts
+  { inFile :: Maybe String
+  , outFile :: Maybe String
+  , unfold :: Bool
+  , pretty :: Bool
+  }
+
+optParser :: Parser Opts
+optParser =
+  Opts
+    <$> (argument (Just <$> str) (metavar "INFILE") <|> pure Nothing)
+    <*> (argument (Just <$> str) (metavar "OUTFILE") <|> pure Nothing)
+    <*> switch (long "unfold" <> short 'u' <> help "Unfold repetitions")
+    <*> switch (long "pretty" <> short 'p' <> help "Pretty-print JSON")
+
+noteToJSON
+  :: (Notation (Pitch p), ToJSON p, ToJSON t, ToJSON i) => NoteId p t i -> Value
+noteToJSON (NoteId p on off id) = object
+  ["pitch" .= showNotationT p, "onset" .= on, "offset" .= off, "id" .= id]
 
 main = do
-  xml <- getContents
-  B.putStr $ encode $ (notateNoteId <$> asNoteWithId <$> xmlNotesHeard xml)
+  let getOpts   = info (optParser <**> helper) fullDesc
+      prettyCfg = P.defConfig { P.confIndent = P.Spaces 1 }
+  opts <- execParser getOpts
+  xml  <- case inFile opts of
+    Just fn -> B.readFile fn
+    Nothing -> B.getContents
+  let
+    notes  = if unfold opts then xmlNotesHeard xml else xmlNotesWritten xml
+    jnotes = noteToJSON <$> asNoteWithId <$> notes
+    str =
+      if pretty opts then P.encodePretty' prettyCfg jnotes else encode jnotes
+  case outFile opts of
+    Just fn -> B.writeFile fn str
+    Nothing -> B.putStr str
